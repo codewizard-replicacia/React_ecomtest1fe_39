cd09830b99c863d0842d7e8494b0626c8503d4ba
Ganapathi Ram <ram@replicacia.com>
2023-11-02 19:52:24 +0530

    initial commit

---
+++ src/utils/display_configuration.js
@@ -0,0 +1,48 @@
+export const productViewConfig = {
+Details: [
+    {
+      key: "Product_id",
+      value: "Product_id",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Name",
+      value: "Name",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Color",
+      value: "Color",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Product_description",
+      value: "Product_description",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Manufacturer",
+      value: "Manufacturer",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Return_merchandise_authorization_nr",
+      value: "Return_merchandise_authorization_nr",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
